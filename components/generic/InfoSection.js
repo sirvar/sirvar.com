edae5919bfe81b5f1c825bcbbf174cc33b65ea01
Rikin Katyal <me@sirvar.com>
2018-07-13 02:59:25 -0400

    Add flex to info cards

--- components/generic/InfoSection.js
+++ components/generic/InfoSection.js
@@ -1,13 +1,22 @@
 import styled from 'styled-components'
 
 const InfoDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-basis: 50%;
+  justify-content: space-between;
+  margin: 0;
+  margin-bottom: 10px;
 `
 
 export default (props) => {
   return (
-    <InfoDiv>
+    <div>
       <h2>{ props.title }</h2>
-      { props.children }
-    </InfoDiv>
+      <InfoDiv>
+        { props.children }
+      </InfoDiv>
+    </div>
   )
 }
