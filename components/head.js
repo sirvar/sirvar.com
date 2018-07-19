d687b984a324606472afff29fee2f19dffb19381
Rikin Katyal <me@sirvar.com>
2018-07-19 17:25:16 -0400

    Google analytics and circleci

--- components/head.js
+++ components/head.js
@@ -8,5 +8,13 @@ export default (props) =>
       <link rel="stylesheet"href="/static/main.css"media="none"onload="this.media='all';" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#25B394" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55807810-5"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-55807810-5');
+      </script>
     </Head>
   </div>
