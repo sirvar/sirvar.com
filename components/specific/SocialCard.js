aa9a33b87df5890dea0f944c314e1d11bfd5811f
Rikin Katyal <me@sirvar.com>
2018-10-06 22:11:49 -0400

    Its something

--- components/specific/SocialCard.js
+++
@@ -1,8 +0,0 @@
-export default (props) => {
-  return (
-    <p>
-      &nbsp;&nbsp;{ props.emoji } <a href={ props.link }>{ props.website }</a>
-      <style jsx>{` margin: 10px 0 !important `}</style>
-    </p>
-  )
-}
