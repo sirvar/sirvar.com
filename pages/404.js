682b99d0cad99f5ed81ad0b0a69a01a7c13be294
Rikin Katyal <katyalrikin@gmail.com>
2020-06-05 01:05:02 -0400

    404

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,13 @@
 import React from "react"
-
 import Layout from "../components/layout"
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  React.useEffect(() => {
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  }, [])
+  return (
+    <></>
+  )
+}
 
 export default NotFoundPage
