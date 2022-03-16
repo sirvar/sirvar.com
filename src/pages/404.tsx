d46414bf46bb740913bbbacc4b740f9edbc865b7
Rikin Katyal <katyalrikin@gmail.com>
2022-03-16 16:04:46 -0500

    revert: 404 page (#30)

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  React.useEffect(() => {
+    window.location.href = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`;
+  }, []);
+  return <></>;
+};
+
+export default NotFoundPage;
