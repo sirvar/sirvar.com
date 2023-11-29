822ac20d8dcd004553b0946b75da0ece9ff0ca07
Rikin Katyal <katyalrikin@gmail.com>
2023-11-29 10:34:34 +0000

    feat: initial travel

---
+++ src/app/travel/components/map.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import Globe, { GlobeMethods } from 'react-globe.gl';
+
+interface Props {
+  data: any;
+  lat: number;
+  lng: number;
+  pinLabel: string;
+}
+
+export default function Map({ data, lat, lng, pinLabel }: Props) {
+  const [width, setWidth] = useState(window?.innerWidth);
+  const [height, setHeight] = useState(window?.innerHeight);
+  const globeEl = useRef<GlobeMethods | undefined>();
+
+  useEffect(() => {
+    if (!globeEl.current) return;
+    // Auto-rotate
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.5;
+
+    const handleResize = () => {
+      setWidth(window?.innerWidth);
+      setHeight(window?.innerHeight);
+    };
+
+    window?.addEventListener(`resize`, handleResize);
+
+    return () => {
+      window?.removeEventListener(`resize`, handleResize);
+    };
+  }, []);
+
+  return (
+    <Globe
+      ref={globeEl}
+      width={width}
+      height={height}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+      bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+      polygonsData={data.features}
+      polygonAltitude={0.005}
+      polygonCapColor={() => `#35446487`}
+      polygonSideColor={() => `#000000FF`}
+      polygonStrokeColor={() => `#000000`}
+      polygonLabel={(country: any) => `
+          <p style="text-shadow: 4px 4px 10px #323264; font-weight: 600;">${country.properties.ADMIN}</p>
+      `}
+      polygonsTransitionDuration={1000}
+      htmlElementsData={[
+        {
+          lat,
+          lng,
+        },
+      ]}
+      htmlElement={() => {
+        const el = document.createElement(`div`);
+        el.innerHTML = `
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: center;">
+          <img src="/images/icons/map.png" style="width: 3rem;margin-top: 0;" />
+          <p style="text-shadow: 4px 4px 10px #323264; font-weight: 600; color: white; font-size: 14px;">${pinLabel}</p>
+        </div>
+        `;
+
+        return el;
+      }}
+    />
+  );
+}
--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,9 +1,61 @@
-export default function Travel() {
+import { get } from '@vercel/edge-config';
+import dynamic from 'next/dynamic';
+
+import Cell from '@/app/components/cell';
+import format from '@/utils/format';
+
+import map from './data/countries.json';
+
+const Map = dynamic(() => import(`@/app/travel/components/map`), {
+  ssr: false,
+});
+
+export const metadata = {
+  title: `Travel`,
+};
+
+async function getCountries() {
+  const locations: unknown[] = (await get(`countriesVisited`)) || [];
+
+  return {
+    ...map,
+    features: map.features.filter((country: any) =>
+      locations.includes(country.properties.ADM0_A3),
+    ),
+  };
+}
+
+export default async function Page() {
+  const distanceFlown: number = (await get(`distanceFlown`)) || 0;
+  const currentCountry: string = (await get(`currentRegion`)) || ``;
+  const currentLocation: string = (await get(`currentLocation`)) || ``;
+  const currentCoordinates = (
+    await (
+      await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+          currentLocation,
+        )}&key=${encodeURIComponent(process.env.OPENCAGE_API_KEY || ``)}`,
+      )
+    ).json()
+  ).results[0].geometry;
+  const data = await getCountries();
+
   return (
-    <main className="md:p-24 p-12">
-      <h1 className="text-5xl text-zinc-800 text-center	font-medium my-24">
-        Travel.
-      </h1>
-    </main>
+    <div className="relative">
+      <Map
+        data={data}
+        lat={currentCoordinates.lat || 0}
+        lng={currentCoordinates.lng || 0}
+        pinLabel={currentLocation}
+      />
+      <div className="fixed flex sm:flex-col px-4 sm:px-0 gap-8 w-full sm:w-auto h-auto sm:h-full justify-center top-4 sm:top-0 sm:right-6 z-10">
+        <Cell
+          heading="Countries Visited"
+          body={data.features.length.toString() || `0`}
+        />
+        <Cell heading="Distance Flown" body={`${format(distanceFlown)} km`} />
+        <Cell heading="Current Country" body={currentCountry} />
+      </div>
+    </div>
   );
 }
