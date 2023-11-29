cb7b4b7f2701d97ae6bb7b5558472de59fc66115
Rikin Katyal <katyalrikin@gmail.com>
2023-11-29 10:40:13 +0000

    wip: travel page

--- src/app/travel/page.tsx
+++ src/app/travel/page.tsx
@@ -1,10 +1,7 @@
-import { get } from '@vercel/edge-config';
-import dynamic from 'next/dynamic';
+import { get } from "@vercel/edge-config";
+import dynamic from "next/dynamic";
 
-import Cell from '@/app/components/cell';
-import format from '@/utils/format';
-
-import map from './data/countries.json';
+import map from "./data/countries.json";
 
 const Map = dynamic(() => import(`@/app/travel/components/map`), {
   ssr: false,
@@ -20,7 +17,7 @@ async function getCountries() {
   return {
     ...map,
     features: map.features.filter((country: any) =>
-      locations.includes(country.properties.ADM0_A3),
+      locations.includes(country.properties.ADM0_A3)
     ),
   };
 }
@@ -33,11 +30,11 @@ export default async function Page() {
     await (
       await fetch(
         `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
-          currentLocation,
-        )}&key=${encodeURIComponent(process.env.OPENCAGE_API_KEY || ``)}`,
+          currentLocation
+        )}&key=${encodeURIComponent(process.env.OPENCAGE_API_KEY || ``)}`
       )
     ).json()
-  ).results[0].geometry;
+  ).results[0]?.geometry;
   const data = await getCountries();
 
   return (
@@ -48,14 +45,14 @@ export default async function Page() {
         lng={currentCoordinates.lng || 0}
         pinLabel={currentLocation}
       />
-      <div className="fixed flex sm:flex-col px-4 sm:px-0 gap-8 w-full sm:w-auto h-auto sm:h-full justify-center top-4 sm:top-0 sm:right-6 z-10">
+      {/* <div className="fixed flex sm:flex-col px-4 sm:px-0 gap-8 w-full sm:w-auto h-auto sm:h-full justify-center top-4 sm:top-0 sm:right-6 z-10">
         <Cell
           heading="Countries Visited"
           body={data.features.length.toString() || `0`}
         />
         <Cell heading="Distance Flown" body={`${format(distanceFlown)} km`} />
         <Cell heading="Current Country" body={currentCountry} />
-      </div>
+      </div> */}
     </div>
   );
 }
