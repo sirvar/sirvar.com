13e2cfe218f73f88b1d1408bff1cdd1b70ea35bd
Rikin Katyal <katyalrikin@gmail.com>
2023-12-01 11:00:57 +0000

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navigation from "@/app/_components/navigation";
 import { GeistSans } from "geist/font/sans";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: {
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Navigation />
         <div className="container md:max-w-2xl mx-auto max-w-none">
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
